ee70528a0d186b87ae1b197dc2d4876fb23fb289
AlexLartsev19 <AlexLartsev19@gmail.com>
2016-02-21 22:37:01 +0300

    Sync lotus

--- pepper.mk
+++ pepper.mk
@@ -62,8 +62,9 @@ PRODUCT_COPY_FILES += \
 # Device specific display resolutions
 # Reference: http://developer.android.com/guide/practices/screens_support.html
 # Note: In PRODUCT_AAPT_PREF_CONFIG set the proper one (e.g. hdpi), in PRODUCT_AAPT_CONFIG set the proper one and the previous one (e.g. mdpi)
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-PRODUCT_AAPT_CONFIG := mdpi hdpi 
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_AAPT_PREBUILT_DPI := mdpi 
 
 
 # Android kind of memory
